27a79d3252fe296f4f2d27f669b038332f6d3515
sebastian <stmr.tay@gmail.com>
2024-12-15 19:07:18 -0800

    Add User login backend endpoint

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -60,7 +60,7 @@ const restoreUser = (req, res, next) => {
     });
 };
 
-  // If there is no current user, return an error
+// If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
 
