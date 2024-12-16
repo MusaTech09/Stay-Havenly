cf82bf7eab0390586148be0e45f06732549197de
sebastian <stmr.tay@gmail.com>
2024-12-15 17:58:54 -0800

    Add an API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,9 @@
+// backend/routes/api/index.js
+const express = require('express');
+const router = express.Router();
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
